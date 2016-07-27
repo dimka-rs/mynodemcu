2bc20517b48b54f6513e2505c92da816c3ebc428
Dmitry K <dimka.rs@gmail.com>
2016-07-27 23:27:14 +0300

    lora: test tx stub

--- lora1278/init.lua
+++ lora1278/init.lua
@@ -18,6 +18,15 @@ function readreg(reg)
   return rdata
 end
 
+function printreg(reg)
+  gpio.write(cs, gpio.LOW)
+  wrote = spi.send(id, reg)
+  rdata = spi.recv(id, 1)
+  gpio.write(cs, gpio.HIGH)
+  print('Reg "'..string.format("0x%02X", reg)..'": '..string.format("0x%02X", string.byte(rdata)..'\n'))
+end
+
+
 function writereg(reg, data)
   gpio.write(cs, gpio.LOW)
   reg=reg+0x80
@@ -26,17 +35,39 @@ function writereg(reg, data)
   gpio.write(cs, gpio.HIGH)
 end
 
-print(string.byte(readreg(0x01))..'\n')
-writereg(0x01, 0x01)
-print(string.byte(readreg(0x01))..'\n')
-writereg(0x01, 0x09)
-print(string.byte(readreg(0x01))..'\n')
-
-print('RegTemp: '..string.format("0x%02X", string.byte(readreg(0x3C)))..'\n')
-
 -- tx init (sleep/stdby)
+writereg(0x01, 0x08) -- fsk modem, sleep mode
+printreg(0x01)
+writereg(0x01, 0x88) -- lora modem, sleep mode
+printreg(0x01)
+writereg(0x01, 0x89) -- lora modem, stdby mode
+printreg(0x01)
+writereg(0x0E, 0x80) -- tx base address
+writereg(0x0F, 0x00) -- rx base address
+
 -- write fifo (stdby)
--- -- Set FifoPtrAddr to FifoTxPtrBase.
--- -- Write PayloadLength bytes to the FIFO (RegFifo)
+writereg(0x0D, 0x80) -- fifo pointer to tx base
+writereg(0x00, 0x0A)
+writereg(0x00, 0x0B)
+writereg(0x00, 0x0C)
+writereg(0x00, 0x0D)
+writereg(0x0D, 0x80) -- fifo pointer to tx base
+for i = 1,8 do
+  printreg(0x00)
+end
+
 -- set mode tx
+writereg(0x01, 0x8B) -- lora modem, tx mode
+printreg(0x01)
+
 -- wait for irq txdone
+tmr.delay(100000)
+printreg(0x12) -- show irq flags, 0x08 = tx done
+writereg(0x12, 0xFF) -- clear all flags
+printreg(0x12) -- show irq flags
+
+-- misc
+printreg(0x18) -- modem status 0x10 = modem clear
+printreg(0x1D) -- modem config 1
+printreg(0x1E) -- modem config 2
+printreg(0x26) -- modem config 3
