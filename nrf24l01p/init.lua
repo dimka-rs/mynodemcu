7ddcb4720f17e832cd53d927f61bd19bd50d6f6a
Dmitry K <dimka.rs@gmail.com>
2016-10-07 20:17:55 +0300

    nrf24: added test regs for default values

--- nrf24l01p/init.lua
+++ nrf24l01p/init.lua
@@ -1,25 +1,55 @@
-pin=0 
-gpio.mode(pin, gpio.OUTPUT)
-spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
+-- NRF24L01 test 
+CONFIG_REG=0x00
+
+csn=0 -- SPI Chip Select
+ce=2  -- NFR Chip Enable
+gpio.mode(csn, gpio.OUTPUT)
+gpio.mode(ce, gpio.OUTPUT)
+gpio.write(csn, gpio.HIGH)
+gpio.write(ce, gpio.LOW)
+spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 8);
+
+function readreg(reg)
+  gpio.write(csn, gpio.LOW)
+  tmr.delay(10)
+  spi.send(1, reg)
+  val = spi.recv(1, 1)
+  gpio.write(csn, gpio.HIGH)
+  return val
+end
 
 function printreg(reg)
-  gpio.write(pin, gpio.LOW)
-  spi.send(1, reg)
-  read1 = spi.recv(1, 1)
-  print('\nReg:'..string.format("0x%02X", reg)..'='..string.format("0x%02X", string.byte(read1)))
-  gpio.write(pin, gpio.HIGH)
+  rreg=readreg(reg)
+  print('\nReg:'..string.format("0x%02X", reg)..'='..string.format("0x%02X", string.byte(rreg)))
+  
 end
 
 function writereg(reg, data)
-  gpio.write(pin, gpio.LOW)
-  tmr.delay(1000)
+  gpio.write(csn, gpio.LOW)
+  tmr.delay(100)
   spi.send(1, reg+0x20, data)
-  gpio.write(pin, gpio.HIGH)
+  gpio.write(csn, gpio.HIGH)
 end
 
 function setrx()
   writereg(0x0,0x0B)
   tmr.delay(150)
+end
+
+
+function test_regs()
+-- reg #   0     1     2     3     4     5     6     7
+  defs={0x08, 0x3F, 0x03, 0x03, 0x03, 0x02, 0x0E, 0x0E}
+  defs_len=8
+  for i=1, defs_len do
+    val=string.byte(readreg(i-1))
+    if val == defs[i] then
+      res="OK  "
+    else
+      res="Fail"
+    end
+    print("Reg "..tostring(i-1).." - ".. res.." reads "..val.."\tmust be "..defs[i])
+  end
 end
 
 -- default values
